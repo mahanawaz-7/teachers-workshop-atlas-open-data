2b6813d8af7496b927c4cee7d6ed2bb57a96dcaf
Mariana Vivas <mariana@MacBook-Pro-4.local>
2024-10-29 15:47:15 +0100

    instead of welcome

---
+++ 00_getting_started.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from utils import load_markdown_file_with_images, get_first_level_headers, load_markdown_preview
+
+def run(selected_tab=None):
+    folder = "getting_started"
+
+    # Initialize session state for expanded state of sections
+    if "expanded_intro" not in st.session_state:
+        st.session_state["expanded_intro"] = False
+    if "expanded_standard_model" not in st.session_state:
+        st.session_state["expanded_standard_model"] = False
+
+    # Get the selected language from session state
+    selected_language = st.session_state.get("language", "english").lower()
+
+    tabs_path = ['00_intro.md']
+
+    # Show full content
+    load_markdown_file_with_images(tabs_path[0], folder, selected_language)
+
+    
